c3cff7dccb77f2e19c91b2855e928515e2aa2883
Bahaa al Halabi <bahaa.alhalabi01@Lau.edu>
2020-08-30 16:09:16 +0300

    delete comment and add comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,6 +8,7 @@ const Post = require('../../models/Post')
 const validatePostInput = require('../../validation/post')
 const Profile = require('../../models/Profile')
 const { response } = require('express')
+const { remove } = require('../../models/Post')
 
 
 // @route       GET api/posts/test
@@ -140,6 +141,67 @@ router.post('/unlike/:id',passport.authenticate('jwt',{session:false}),(req,res)
 })
 
 
+// @route       POST api/posts/comment/:id ( post id)
+// @description Add comment to a post
+// @access      private
+router.post('/comment/:id',passport.authenticate('jwt',{session:false}),(req,res) =>{
+
+    const {errors, isValid} = validatePostInput(req.body)
+
+    //validation
+    if(!isValid){
+        return res.status(400).json(errors)
+    }
+
+
+    Post.findById(req.params.id).then(post =>{
+        const newComment = {
+            text:req.body.text,
+            name:req.body.name,
+            avatar:req.body.avatar,
+            user:req.user.id
+        }
+
+        //add to the comments array
+        post.comments.unshift(newComment)
+        post.save().then(post => res.json(post))
+    }).catch(err => res.status(404).json(err))
+
+
+})
+
+
+// @route       DELTE api/posts/comment/:id/:comment_id ( post id)
+// @description Delete comment to a post by its id
+// @access      private
+router.delete('/comment/:id/:comment_id',passport.authenticate('jwt',{session:false}),(req,res) =>{
+
+    const {errors, isValid} = validatePostInput(req.body)
+
+    //validation
+    if(!isValid){
+        return res.status(400).json(errors)
+    }
+
+
+    Post.findById(req.params.id).then(post =>{
+
+        //check to see if the comment exists
+        if(post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0){
+            return res.status(404).json({commentnotexist:'Comment does not exist'})
+        }
+
+        //get the index to remove
+        const removeIndex = post.comments.map(item => item._id.toString()).indexOf(req.params.comment_id)
+        
+        //remove it from the array
+        post.comments.splice(removeIndex,1)
+        post.save().then(post => res.json(post))
+
+    }).catch(err => res.status(404).json(err))
+
+
+})
 
 
 
