b07559d64ea8bb7938d4df76782eb2d1195c9fe3
Bahaa al Halabi <bahaa.alhalabi01@Lau.edu>
2020-09-03 23:30:28 +0300

    create profile component route

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Register from "./components/auth/Register"
 import Login from "./components/auth/Login"
 import Dashboard from "./components/dashboard/Dashboard"
 import PrivateRoute from "./components/common/PrivateRoute"
+import CreateProfile from "./components/create-profile/CreateProfile"
 
 import "./App.css"
 import { clearCurrentProfile } from "./actions/profileActions"
@@ -50,6 +51,9 @@ function App() {
             <Route exact path='/login' component={Login} />
             <Switch>
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+            </Switch>
+            <Switch>
+              <PrivateRoute exact path='/create-profile' component={CreateProfile} />
             </Switch>
           </div>
           <Footer />
